62512a58d98ab3b4083765404b56518f6f028908
Ludvig Lundgren <lulundg@kth.se>
2023-03-14 09:07:26 +0800

    test
    test

---
+++ script.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Dataset 1
+player = pd.read_csv("./players.csv")
+club = pd.read_csv("./clubs.csv")
+value = pd.read_csv("./player_valuations.csv")
+
+# Dataset 2
+stat = pd.read_csv("./2021-2022 Football Player Stats.csv")
+
