eefac8d109b6e2893cd84134543a67a33747c44a
twoeasy3 <65521492+twoeasy3@users.noreply.github.com>
2023-04-20 22:30:44 +0800

    Added Plotly display for best value squad

--- script/pages/2_Valuation_Comparison.py
+++ script/pages/2_Valuation_Comparison.py
@@ -47,11 +47,77 @@ df = df.sort_values(['Comp','Squad','Player'],
 #player = st.selectbox("Select the player to analyze", pd.unique(df.loc[df['Squad'] == pClub, 'Player']))
 #placeholder = st.empty()
 
-info = pd.DataFrame(df[["name", 'Comp', 'Squad']])
+info = pd.DataFrame(df[["name", 'Comp', 'Squad','Pos']])
 pred_Y = pd.DataFrame(pred_Y, columns = ["predicted_market_value_in_eur"])
 
 comparison = info.join(Y).join(pred_Y).round().reset_index(drop = True)
 comparison["Undervalued(+) / Overvalued(-)"] = round((comparison["predicted_market_value_in_eur"]/comparison["market_value_in_eur"] - 1)*100, 2)
 
 st.write(comparison)
+
+#////This entire code here is for the best value 11
+st.write("### Best Value XI")
+st.write("Picking the most undervalued players in a standard 4-4-2 formation, our algorithm deems this squad to be the best value for money")
+xCoord = [0.25,0.5,0,0.25,0.5,0.75,0,0.25,0.5,0.75,0.375]
+yCoord = [1,1,0.75,0.75,0.75,0.75,0.5,0.5,0.5,0.5,0.25]
+valueEleven = ['Moise Kean','Joel Pohjanpalo','Baptiste Santamaria','Cesc Fàbregas','Kalvin Phillips',
+              'Wilfred Ndidi','Chris Richards','Leonardo Spinazzola','Lennard Maloney','Andrea Conti','David Raya']
+
+
+selectedPlayers = df['Player'].isin(valueEleven)
+df11 = df[selectedPlayers]
+df11 = df11.assign(xCoord = xCoord)
+df11 = df11.assign(yCoord = yCoord)
+
+fig = px.scatter(df11,
+                x = 'xCoord', 
+                y = 'yCoord', 
+                hover_name = "Player",
+                width = 600,
+                height = 800,
+                opacity = 0,
+                hover_data={'xCoord':False,'yCoord':False,'Squad':True,'Comp':True},
+                color_discrete_sequence=px.colors.qualitative.G10)
+
+
+fig.update_layout(
+    coloraxis_showscale=False,
+    xaxis=dict(showgrid=False, zeroline=False),
+    yaxis=dict(showgrid=False, zeroline=False),
+)
+
+fig.update_xaxes(showticklabels=False,range = [-0.125,0.875])
+fig.update_yaxes(showticklabels=False,range = [0.110,1.125])
+fig.update_xaxes(title_text='')
+fig.update_yaxes(title_text='')
+fig.update_traces(marker={'size': 100})
+
+import base64
+pitch = base64.b64encode(open(os.path.dirname(os.getcwd())+"/data/images/data/pitch.jpg",'rb').read())
+fig.update_layout(images = [dict(source='data:image/jpg;base64,{}'.format(pitch.decode()), xref="paper", yref="paper",
+                    x=0, y=1,
+                    sizex=1, sizey=1,
+                    xanchor="left",
+                    yanchor="top",
+                    sizing="stretch",
+                    layer="below")])
+
+for index, row in df11.iterrows():
+    imageLocation = os.path.dirname(os.getcwd())+'/data/images/players/'+row['Player'] +'.png'
+    imageEncode = base64.b64encode(open(imageLocation,'rb').read())
+    fig.add_layout_image(dict(source='data:image/png;base64,{}'.format(imageEncode.decode()),
+                        x=row['xCoord']+0.125,
+                        y=row['yCoord']-0.1,
+                        xref='paper',
+                        yref='paper',
+                        sizex=0.20, sizey=0.20,
+                        xanchor="center",
+                        yanchor="middle",
+                        layer="above"))
+
+
+
+
+fig.update_layout()
+st.plotly_chart(fig)
 
